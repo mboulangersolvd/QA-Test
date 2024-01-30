6102ffa8193c703d3126659665791eb157735c90
magaliboulanger <56554227+magaliboulanger@users.noreply.github.com>
2024-01-30 15:32:00 -0300

    Register Test case implemented.

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,13 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import pages.HomePage;
+
+public class BaseTest {
+    public HomePage openHomePage() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.automationexercise.com");
+        return new HomePage();
+    }
+}
